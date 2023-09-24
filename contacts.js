7308e267a6fb55d3fdc044e9233749ea7de00d52
OlehKozubJS <oleg.kozub54@gmail.com>
2023-09-24 16:57:04 +0300

    Update contacts.js

--- contacts.js
+++ contacts.js
@@ -4,3 +4,26 @@ const path = require("path");
 const contactPath = require("./db/contacts.json");
 
 const { readFile, writeFile } = fs;
+// contacts.js
+
+/*
+ * Розкоментуй і запиши значення
+ * const contactsPath = ;
+ */
+
+// TODO: задокументувати кожну функцію
+function listContacts() {
+  // ...твій код. Повертає масив контактів.
+}
+
+function getContactById(contactId) {
+  // ...твій код. Повертає об'єкт контакту з таким id. Повертає null, якщо контакт з таким id не знайдений.
+}
+
+function removeContact(contactId) {
+  // ...твій код. Повертає об'єкт видаленого контакту. Повертає null, якщо контакт з таким id не знайдений.
+}
+
+function addContact(name, email, phone) {
+  // ...твій код. Повертає об'єкт доданого контакту.
+}
