a6fff8787443ac6a0a80428cad98bd27149382d7
OlehKozubJS <oleg.kozub54@gmail.com>
2023-09-24 23:23:03 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,2 +1,30 @@
 const fs = require("fs/promises");
 const path = require("path");
+// index.js
+const argv = require("yargs").argv;
+
+// TODO: рефакторити
+function invokeAction({ action, id, name, email, phone }) {
+  switch (action) {
+    case "list":
+      // ...
+      break;
+
+    case "get":
+      // ... id
+      break;
+
+    case "add":
+      // ... name email phone
+      break;
+
+    case "remove":
+      // ... id
+      break;
+
+    default:
+      console.warn("\x1B[31m Unknown action type!");
+  }
+}
+
+invokeAction(argv);
