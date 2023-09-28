f2c2eeda99b470c9802de1690607a626652a2fe7
OlehKozubJS <oleg.kozub54@gmail.com>
2023-09-28 23:56:57 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,18 +1,3 @@
-const contacts = require("./contacts");
-
-const { Command } = require("commander");
-const program = new Command();
-const argv = program.opts();
-/*
-program
-  .option("-a, --action <type>", "choose action")
-  .option("-i, --id <type>", "user id")
-  .option("-n, --name <type>", "user name")
-  .option("-e, --email <type>", "user email")
-  .option("-p, --phone <type>", "user phone");
-
-program.parse(process.argv);
-*/
 // TODO: рефакторити
 async function invokeAction({ action, id, name, email, phone }) {
   switch (action) {
@@ -38,5 +23,3 @@ async function invokeAction({ action, id, name, email, phone }) {
       console.warn("\x1B[31m Unknown action type!");
   }
 }
-
-invokeAction(argv);
