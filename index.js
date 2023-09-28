71ae7166c2dda049cd5104134f61649c9f3ea6cb
OlehKozubJS <oleg.kozub54@gmail.com>
2023-09-28 23:37:39 +0300

    Update index.js

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ async function invokeAction({ action, id, name, email, phone }) {
       break;
 
     case "get":
-      const contact = find();
+      const contact = contacts.getContactById;
       break;
 
     case "add":
