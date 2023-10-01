8f7d715326aaf33955a46a110dd59500d4ba4632
OlehKozubJS <oleg.kozub54@gmail.com>
2023-10-01 12:13:17 +0300

    Update index.js

--- db/index.js
+++ db/index.js
@@ -3,7 +3,7 @@ const path = require("path");
 
 const { readFile, writeFile } = fs;
 
-const contactsPath = path.resolve("db", "contacts.json");
+const contactsPath = path.resolve("db", "contacts.js");
 
 const listContacts = async () => {
   const data = await readFile(contactsPath, "utf-8");
