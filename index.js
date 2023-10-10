31c20df78e571e8cec3c808df720c1ff8561606d
OlehKozubJS <oleg.kozub54@gmail.com>
2023-10-10 12:07:35 +0300

    npm i yargs

--- index.js
+++ index.js
@@ -47,9 +47,26 @@ async function invokeAction({ action, id, name, email, phone }) {
 }
 
 invokeAction(argv);
-
+/*
 const actionIndex = process.argv.indexOf("--action");
 if (actionIndex !== -1) {
   const action = process.argv(actionIndex + 1);
   console.log(action);
 }
+*/
+
+/*
+
+# Отримуємо і виводимо весь список контактів у вигляді таблиці (console.table)
+node index.js --action="list"
+
+# Отримуємо контакт по id і виводимо у консоль об'єкт контакту або null, якщо контакту з таким id не існує.
+node index.js --action="get" --id 05olLMgyVQdWRwgKfg5J6
+
+# Додаємо контакт та виводимо в консоль об'єкт новоствореного контакту
+node index.js --action="add" --name Mango --email mango@gmail.com --phone 322-22-22
+
+# Видаляємо контакт та виводимо в консоль об'єкт видаленого контакту або null, якщо контакту з таким id не існує.
+node index.js --action="remove" --id qdggE76Jtbfd9eWJHrssH
+
+*/
